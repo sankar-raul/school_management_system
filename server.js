5a3fb1ef3e67d6f88d3d83640df97c559191b1f2
Sankar <raulsankar99@gmail.com>
2024-05-07 11:35:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
+const port = process.env.port || 8080
 const students = require("./routes/students")
 app.use(express.static("static"))
 app.use("/students", students)
-app.listen(8080, () => {
-    console.log("Server is running at http://localhost:8080")
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
 })
