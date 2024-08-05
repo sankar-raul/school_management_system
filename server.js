72239412ca017eea8983e0d023fe1b346ddde593
sankar-raul <raulsankar99@gmail.com>
2024-08-06 02:47:46 +0530

    3am

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const cookieParser = require('cookie-parser')
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 const students = require("./routes/students")
 const home = require('./routes/home')
 const dashboard = require('./routes/dashboard')
